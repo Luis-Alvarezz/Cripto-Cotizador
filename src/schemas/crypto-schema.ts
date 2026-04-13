32da4a04d3b76657bf2c8a9f7c0907f5f2dfa9b5
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-12 22:04:28 -0600

    [MODIFY]: Esquema para obtener datos de API de forma separada en Objeto y Array

--- src/schemas/crypto-schema.ts
+++ src/schemas/crypto-schema.ts
@@ -5,3 +5,11 @@ export const CurrencySchema = z.object({
   code: z.string(),
   name: z.string()
 })
+
+// ! 1.- Metodo para Crear ESQUEMA de la Cripto con el Codigo d 3 digitos y el nombre
+export const CryptoCurrencyResponseSchemaAPI = z.object({
+  NAME: z.string(),
+  SYMBOL: z.string()
+})
+
+export const CryptoCurrenciesResponseSchemaAPI = z.array(CryptoCurrencyResponseSchemaAPI) // * Similar a Product[]
