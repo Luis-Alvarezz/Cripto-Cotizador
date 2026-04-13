7cbc37a500d89c438999abfb0d1f03a179e1e10c
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-12 22:07:00 -0600

    [MODIFY]: Se infiere respuesta de API para las Criptomonedas

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,5 +1,9 @@
 import z from 'zod'
-import { CurrencySchema } from '../schemas/crypto-schema'
+import { CryptoCurrencyResponseSchemaAPI, CurrencySchema } from '../schemas/crypto-schema'
 
 // ! 2.- Infiriendo el TYPE mediente el ESQUEMA
 export type Currency = z.infer<typeof CurrencySchema>
+
+// ! 2.- Infiriendo el TYPe mediante ESQUEMA para respuesta de la API
+// export type CryptoCurrenciesInfer = z.infer<typeof CryptoCurrenciesResponseSchemaAPI>
+export type CryptoCurrencyInfer = z.infer<typeof CryptoCurrencyResponseSchemaAPI>
