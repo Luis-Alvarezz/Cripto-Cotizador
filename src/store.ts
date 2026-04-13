884dba6436f5b08143cf6ebf4f89ca452de67b4f
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-12 22:33:15 -0600

    [MODIFY]: Cambio de Llamado a API a Servicios

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,8 @@
 // * Similar a useReducer donde colocaremos STATE y Dispatch (Metodos a Utilizar)
 import { create } from "zustand";
-import axios from "axios";
+import { devtools } from "zustand/middleware";
 import type { CryptoCurrencyInfer } from "./types/type";
-import { CryptoCurrenciesResponseSchemaAPI } from "./schemas/crypto-schema";
+import { getCriptos } from "./services/CryptoService";
 
 type CryptoStoreType = {
   // cryptoCurrencies: CryptoCurrenciesInfer // * Lo mismo a la sig linea:
@@ -10,22 +10,7 @@ type CryptoStoreType = {
   fetchCryptos: () => Promise<void>
 }
 
-async function getCriptos() {
-  const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD&page_size=20'
-  // const data = await axios(url)
-  // console.log(data);
-  // console.log(data.data);
-  // console.log(data.data.Data.LIST); // * EQUIVALENTE A:
-  const {data : { Data: {LIST} } } = await axios(url)
-  // console.log(LIST);
-  const result = CryptoCurrenciesResponseSchemaAPI.safeParse(LIST) // * Revisamos la respuesta de la API porque TS ya no puede mediante el ESQUEMA
-  console.log(result);
-  if (result.success === true) {
-    return result.data
-  }
-}
-
-export const useCryptoStore = create<CryptoStoreType>((set) => ({
+export const useCryptoStore = create<CryptoStoreType>()(devtools((set) => ({
   // * Colocar STATE y funciones que modifican al STATE (dispatch)
   cryptoCurrencies: [],
   fetchCryptos: async () => {
@@ -38,4 +23,4 @@ export const useCryptoStore = create<CryptoStoreType>((set) => ({
       cryptoCurrencies: cryptoCurrencies
     }))
   }
-}))
+})))
