c1cc6fbac7461173eb029d356bd6aa4321bdd619
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-13 21:55:41 -0600

    [ADD]: Se infiere el Esquema para el STATE de Formulario

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,9 +1,12 @@
 import z from 'zod'
-import { CryptoCurrencyResponseSchemaAPI, CurrencySchema } from '../schemas/crypto-schema'
+import { CryptoCurrencyResponseSchemaAPI, CurrencySchema, PairSchema } from '../schemas/crypto-schema'
 
 // ! 2.- Infiriendo el TYPE mediente el ESQUEMA
 export type Currency = z.infer<typeof CurrencySchema>
 
-// ! 2.- Infiriendo el TYPe mediante ESQUEMA para respuesta de la API
+// ! 2.1- Infiriendo el TYPe mediante ESQUEMA para respuesta de la API
 // export type CryptoCurrenciesInfer = z.infer<typeof CryptoCurrenciesResponseSchemaAPI>
 export type CryptoCurrencyInfer = z.infer<typeof CryptoCurrencyResponseSchemaAPI>
+
+// ! 2.2- Infiriendo el TYPE mediante ESQUEMA para el STATE de 'pair'
+export type PairInfer = z.infer<typeof PairSchema> 
