8cfdb7dcf015a756d5e82aa02a9d809e4c0bc6a7
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-13 21:01:21 -0600

    [ADD]: Esquema para STATE de Formulario con su Infer

--- src/schemas/crypto-schema.ts
+++ src/schemas/crypto-schema.ts
@@ -6,10 +6,16 @@ export const CurrencySchema = z.object({
   name: z.string()
 })
 
-// ! 1.- Metodo para Crear ESQUEMA de la Cripto con el Codigo d 3 digitos y el nombre
+// ! 1.1- Metodo para Crear ESQUEMA de la Cripto con el Codigo d 3 digitos y el nombre
 export const CryptoCurrencyResponseSchemaAPI = z.object({
   NAME: z.string(),
   SYMBOL: z.string()
 })
 
 export const CryptoCurrenciesResponseSchemaAPI = z.array(CryptoCurrencyResponseSchemaAPI) // * Similar a Product[]
+
+// ! 1.2- Metodo crear ESQUEMA en el STATE de pair (CriptoSearchForm):
+export const PairSchema = z.object({
+  currency: z.string(),
+  cryptocurrency: z.string()
+})
