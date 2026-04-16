10f1e7a6a2c67f9f9bd1ec53ff4bdcd612f13ec4
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-15 21:22:25 -0600

    [ADD]: Accion para cargar la corizacion de Moneda a Crypto con llamado a Service para Endpoint setteando en STATE

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,38 @@
 // * Similar a useReducer donde colocaremos STATE y Dispatch (Metodos a Utilizar)
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import type { CryptoCurrencyInfer } from "./types/type";
-import { getCriptos } from "./services/CryptoService";
+import type { APIResponseInfer, CryptoCurrencyInfer, PairInfer } from "./types/type";
+import { fetchCurrentCryptoPrice, getCriptos } from "./services/CryptoService";
 
 type CryptoStoreType = {
   // cryptoCurrencies: CryptoCurrenciesInfer // * Lo mismo a la sig linea:
   cryptoCurrencies: CryptoCurrencyInfer[]
+  APIResponse: APIResponseInfer
   fetchCryptos: () => Promise<void>
+  fetchData: (pair: PairInfer) => Promise<void>
 }
 
 export const useCryptoStore = create<CryptoStoreType>()(devtools((set) => ({
   // * Colocar STATE y funciones que modifican al STATE (dispatch)
   cryptoCurrencies: [],
+  APIResponse: {} as APIResponseInfer, // * Este OBJETO Tratalo como APIResponseInfer
   fetchCryptos: async () => {
     // console.log('Desde FetchCrypto');
     // * Mandar a llamar el JSON y obtener las 20 monedas mas valiosas
     const cryptoCurrencies = await getCriptos() // * Obtenemos Promise {<pending>} | Necesitamos convertir esta accion en ASINCRONA 
-    console.log(cryptoCurrencies);
+    // console.log(cryptoCurrencies);
     // ! Escribir en el STATE
     set(() => ({
       cryptoCurrencies: cryptoCurrencies
+    }))
+  },
+
+  fetchData: async (pair) => {
+    // console.log('Desde FetchData, recibiendo Pair: ',pair);
+    const APIResponse = await fetchCurrentCryptoPrice(pair)
+    // console.log(APIResponse); // * OUTPUT: Misma respuesta del Service en base al ESQUEMA de la API Response
+    set(() => ({
+      APIResponse: APIResponse
     }))
   }
 })))
